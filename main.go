ff70eb4b041eff4909b2368dfa3067295ed4732f
DeepakGunpal <gunpaldeepak@gmail.com>
2024-05-08 09:24:55 +0530

    added stringutils package

--- main.go
+++ main.go
@@ -3,23 +3,16 @@ package main
 
 import (
 	"fmt"
-)
 
-// stringReverse returns the reverse of the given string.
-func stringReverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
+	"github.com/DeepakGunpal/stringReverser/pkg/stringutils"
+)
 
 func main() {
 	fmt.Println("Cheetah Clash")
 
 	// Example usage of stringReverse
 	original := "Hello, World!"
-	reversed := stringReverse(original)
+	reversed := stringutils.Reverse(original)
 	fmt.Printf("Original: %s\nReversed: %s\n", original, reversed)
 
 	// Your main application logic goes here
